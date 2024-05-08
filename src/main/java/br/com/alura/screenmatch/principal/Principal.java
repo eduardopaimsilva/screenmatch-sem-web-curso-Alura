2fa8392cadcfc59c1a6c34cd10588cb1e687fe9b
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-08 00:32:29 -0300

    calcular estatísticas como média, classe DoubleSummaryStatistics

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -104,5 +104,13 @@ public class Principal {
                         Collectors.averagingDouble(Episodio::getAvaliacao)));
         System.out.println(avaliacoesPorTemporada);
 
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 00)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Media: "+est.getAverage());
+        System.out.println("Melhor episódio: "+ est.getMax());
+        System.out.println("Pior episódio: "+ est.getMin());
+        System.out.println("Quantidade: "+ est.getCount());
+
     }
 }
